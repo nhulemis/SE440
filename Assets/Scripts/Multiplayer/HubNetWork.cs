f2cf2de4e11a9a9cb34b6f667ffeaec782e416e9
dev-heole-148 <heole@nevel.tech>
2025-07-05 16:13:52 +0700

    Demo multiplayer with photon PUN

---
+++ Assets/Scripts/Multiplayer/HubNetWork.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Multiplayer
+{
+    public class HubNetWork : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] Button joinRoomButton;
+        [SerializeField] Button leaveRoomButton;
+        [SerializeField] GameObject roomItemPrefab;
+        private void Start()
+        {
+            leaveRoomButton.gameObject.SetActive(false);
+            joinRoomButton.gameObject.SetActive(false);
+            if (PhotonNetwork.IsConnected)
+            {
+                Debug.Log("Already connected to Photon Network.");
+            }
+            else
+            {
+                Debug.Log("Connecting to Photon Network...");
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Connected to Photon Master Server.");
+            PhotonNetwork.JoinLobby();
+        }
+        
+        public override void OnJoinedLobby()
+        {
+            Debug.Log("Joined Photon Lobby.");
+            joinRoomButton.gameObject.SetActive(true);
+        }
+
+        public void JoinRoom()
+        {
+            PhotonNetwork.JoinRandomOrCreateRoom();
+        }
+        
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined a room successfully.");
+            joinRoomButton.gameObject.SetActive(false);
+            leaveRoomButton.gameObject.SetActive(true);
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            base.OnRoomListUpdate(roomList);
+            Debug.Log("Room list updated. Number of rooms: " + roomList.Count);
+            Transform roomListParent = roomItemPrefab.transform.parent;
+            while (roomListParent.childCount > 1)
+            {
+                Destroy(roomListParent.GetChild(1).gameObject);
+            }
+            foreach (RoomInfo room in roomList)
+            { 
+                var go = Instantiate(roomItemPrefab, roomListParent);
+                go.name = room.Name;
+                go.SetActive(true);
+            }
+        }
+
+        public void LeaveRoom()
+        {
+            if (PhotonNetwork.InRoom)
+            {
+                PhotonNetwork.LeaveRoom();
+                leaveRoomButton.gameObject.SetActive(false);
+                joinRoomButton.gameObject.SetActive(true);
+                Debug.Log("Left the room.");
+            }
+            else
+            {
+                Debug.LogWarning("Not currently in a room to leave.");
+            }
+        }
+    }
+}
